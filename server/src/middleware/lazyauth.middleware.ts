e16fd17bd0b7f4e47c48370e4ff5b73296b6a8c0
Dhvanitmonpara <monparadhvanit@gmail.com>
2025-05-09 11:08:47 +0530

    updated lazycheck

--- server/src/middleware/lazyauth.middleware.ts
+++ server/src/middleware/lazyauth.middleware.ts
@@ -1,9 +1,12 @@
 import { NextFunction, Request, Response } from "express";
-import userModel from "../models/user.model.js";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { env } from "../conf/env.js";
 
-const verifyJWTLazyCheck = async (req: Request, res: Response, next: NextFunction) => {
+const verifyJWTLazyCheck = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const token =
       req.cookies?.__accessToken ||
@@ -15,32 +18,22 @@ const verifyJWTLazyCheck = async (req: Request, res: Response, next: NextFunctio
 
     const decodedToken = jwt.verify(token, env.accessTokenSecret) as JwtPayload;
 
-    const user = await userModel
-      .findById(decodedToken?._id)
-      .select("-password -refreshToken -email")
-      .lean();
-
-    if (!user) {
-      return next();
-    }
-
-    const mappedUser = {
-      _id: user._id,
-      username: user.username,
-      isVerified: user.isVerified,
-      isBlocked: user.isBlocked,
-      suspension: {
-        ends: user.suspension?.ends ?? null,
-        reason: user.suspension?.reason ?? null,
-        howManyTimes: user.suspension?.howManyTimes ?? 0,
+    req.user = {
+      _id: decodedToken._id,
+      username: decodedToken.username,
+      isVerified: decodedToken.isVerified,
+      isBlocked: decodedToken.isBlocked,
+      suspension: decodedToken.suspension ?? {
+        ends: null,
+        reason: null,
+        howManyTimes: 0,
       },
       refreshToken: null,
-      bookmarks: user.bookmarks,
-      branch: user.branch ?? "",
-      college: user.college ?? null,
+      bookmarks: [],
+      branch: "",
+      college: null,
     };
 
-    req.user = mappedUser;
     next();
   } catch (error) {
     next();
